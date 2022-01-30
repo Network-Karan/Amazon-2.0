dff26b0d56cf122cd8a2afcf01dacb5264672cb9
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2022-01-30 17:49:57 +0530

    init

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuJc1RF9Ft-Tnq3buAZZKNBKzBpCbB5_4",
+    authDomain: "clone-509bb.firebaseapp.com",
+    projectId: "clone-509bb",
+    storageBucket: "clone-509bb.appspot.com",
+    messagingSenderId: "388907999371",
+    appId: "1:388907999371:web:3062c78c1e3644b7d8748e",
+    measurementId: "G-MGLTRBR03B"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
