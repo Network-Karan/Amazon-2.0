b9df72cfce770f7d1509a5df792a4317db477617
Network-Karan <kaliyaperumal.diwakaran@gmail.com>
2022-01-30 21:09:14 +0530

    init

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-function getItems(){
-    db.collection("users").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-        });
-    });
-}
